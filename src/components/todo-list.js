cc0c68256394e62a6150bac54ed9c60791f2acf9
Salavat F <salmrproperfahr@mail.ru>
2022-02-14 11:02:11 +0300

    add css to project

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,24 +1,25 @@
 import React from 'react';
 
 import TodoListItem from './todo-list-item';
+import './todo-list.css';
 
-const TodoList = ({todos}) => {
+const TodoList = ({ todos }) => {
 
-    const elements = todos.map((item) => {
-        
-        const {id, ...itemProps} = item; // чтобы не передавать лишний id в todolistitem, отделили id и itemprops
-        return (
-            <li key={id}>
-                <TodoListItem {...itemProps} />
-            </li> 
-        );
-    });
+  const elements = todos.map((item) => {
+    const { id, ...itemProps } = item;
 
     return (
-        <ul>
-            {elements}
-        </ul>
+      <li key={id} className="list-group-item">
+        <TodoListItem {...itemProps } />
+      </li>
     );
-}
+  });
+
+  return (
+    <ul className="list-group todo-list">
+      { elements }
+    </ul>
+  );
+};
 
 export default TodoList;
