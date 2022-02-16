b3274851ced6aba4641573d82e9cc897a1d4fd13
Salavat F <salmrproperfahr@mail.ru>
2022-02-16 17:24:15 +0300

    done adding todos

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,15 +3,36 @@ import React, {Component} from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label);
+        this.setState({
+            label: ''
+        });
+    };
+
     render() {
         return (
-            <div className="item-add-form">
-                <button 
-                    className='btn btn-outline-secondary'
-                    onClick={()=>this.props.onItemAdded('hello world')}>
+            <form className="item-add-form d-flex"
+                onSubmit={this.onSubmit}>
+                <input type="text" className='form-control' placeholder="What needs to do?"
+                    onChange={this.onLabelChange} 
+                    value={this.state.label}/>
+                <button className='btn btn-outline-secondary'>
                     Add Item
                 </button>
-            </div>
+            </form>
         )
-    }
-}
+    };
+};
